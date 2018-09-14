dcc04f6f6284c8b674f0b2ae3f78737d0172be4e
nj <njsung1217@gmail.com>
2018-09-14 09:17:03 +0900

    180914
    first commit
    -백----업

--- main.cpp
+++ main.cpp
@@ -272,8 +272,7 @@ int main(void)
 			render->invoke_collisionBB_shader();
 			
 		}
-			render->render();
-		
+			render->render();		
 	} //Check if the ESC key had been pressed or if the window had been closed
 	
 	while (!glfwWindowShouldClose(window));		
